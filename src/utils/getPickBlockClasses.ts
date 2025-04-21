f401595dd64085e5fc1f1ad2e30f88765e056448
jijimka <jjeam6thegod@mail.ru>
2025-04-21 22:09:46 +0500

    focusPick v1

---
+++ src/utils/getPickBlockClasses.ts
@@ -0,0 +1,40 @@
+//if (!picks.includes(orderNumber)) {
+//         blockClasses.push('pick__block-ban')
+//     } else {
+//         blockClasses.push('pick__block')
+//     }
+//     if (pickQueue[0] === orderNumber) {
+//         blockClasses.push('pick-block__active')
+//     } else if (pickOrder === orderNumber && pickQueue.length === 0) {
+//         blockClasses.push('pick-block__active')
+//     }
+//     if (dire.includes(orderNumber)) {
+//         orderClasses.push('direOrder')
+//     }
+
+
+
+
+
+
+const picks = [8,9,13,14,15,16,17,18,23,24]
+// const dire = [2,3,5,6,9,12,13,16,17,20,21,24]
+
+export function usePickBlockClasses(orderNumber:number, pickOrder:number, selectedPick:number | null, pickQueue:number[]):string[] {
+    const blockClasses: string[] = []
+        if (!picks.includes(orderNumber)) {
+            blockClasses.push('pick__block-ban')
+        } else {
+            blockClasses.push('pick__block')
+        }
+        if (selectedPick !== null && selectedPick === orderNumber) {
+            blockClasses.push('pick-block__selected')
+        }
+        if (pickQueue[0] === orderNumber) {
+            blockClasses.push('pick-block__active')
+        } else if (pickOrder === orderNumber && pickQueue.length === 0) {
+            blockClasses.push('pick-block__active')
+        }
+    return blockClasses
+
+}
