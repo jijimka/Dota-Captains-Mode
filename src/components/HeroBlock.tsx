eafdaffa2f577ea040f9f456ad2e3d4396b85b36
jijimka <jjeam6thegod@mail.ru>
2025-04-23 21:31:14 +0500

    banned heroes are marked and edited pickedHeroSlice

--- src/components/HeroBlock.tsx
+++ src/components/HeroBlock.tsx
@@ -6,28 +6,46 @@ interface HeroBlockProps {
     hero:IHeroes,
 }
 const HeroBlock:FC<HeroBlockProps> = ({hero}) => {
-    const {disabledHeroes} = useTypedSelector(state => state.pickedHeroes)
+    const {pickedHeroes} = useTypedSelector(state => state.pickedHeroes)
     const {searchedHero} = useTypedSelector(state => state.heroes)
     const {addConfirmHero} = pickedHeroSlice.actions
     const blockClasses:string[] = []
     const dispatch = useTypedDispatch()
 
+    function isHeroPicked():boolean {
+        return pickedHeroes.find((pickedHero) => pickedHero.hero.id === hero.id) !== undefined
+    }
+    function isHeroBanned() {
+        const picks = [8,9,13,14,15,16,17,18,23,24]
+        const foundHero = pickedHeroes.find((pickedHero) => pickedHero.hero.id === hero.id)
+        if (foundHero === undefined ) return false
+        return !picks.includes(foundHero.pick)
+    }
     function heroClick(hero:IHeroes) {
-        if (disabledHeroes.includes(hero.id)) return
+        if (isHeroPicked()) return
         dispatch(addConfirmHero(hero))
     }
 
-    if (disabledHeroes.includes(hero.id)) {
-        blockClasses.push('heroDisabled')
-    } else {
+    if (isHeroPicked()) {
+        if (isHeroBanned()) {
+            blockClasses.push('heroBanned')
+        } else {
+            blockClasses.push('heroDisabled')
+        }
+    }  else {
         blockClasses.push('attribute__image-container')
     }
+
+
+
     if (searchedHero) {
         if (!searchedHero.includes(hero.id)) {
             blockClasses.push('not-searched')
         }
     }
+
     return (
+
         <div onClick={() => heroClick(hero)} className={blockClasses.join(' ')}>
             <img src={hero.image}
                  className='image-container__image'
