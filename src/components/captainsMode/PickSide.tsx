ceb097c817d6c9ed861fc83b86d10e421f1501dd
jijimka <jjeam6thegod@mail.ru>
2025-09-09 17:37:31 +0500

    done

--- src/components/captainsMode/PickSide.tsx
+++ src/components/captainsMode/PickSide.tsx
@@ -1,12 +1,28 @@
-import {FC} from 'react';
+import {FC, useMemo} from 'react';
 import PickBlock from "./PickBlock.tsx";
 import {PickOrder} from "../../models/PickOrder.ts";
+import {useTypedSelector} from "../../hooks/redux.ts";
 
 interface PickSideProps {
     side: 'Radiant' | 'Dire'
 }
 
 const PickSide: FC<PickSideProps> = ({side}) => {
+    const {selectedPick, pickQueue} = useTypedSelector(state => state.pickOrder)
+
+    const sideTitleClasses = useMemo(() => {
+        const isRadiantTurn = PickOrder.radiant.includes(selectedPick ?? pickQueue[0])
+        const array: string[] = [side === 'Radiant' ? 'pick-side__title-radiant' : 'pick-side__title-dire']
+
+        if (isRadiantTurn && side === 'Radiant') {
+            array.push('title__active-radiant')
+        }
+        if (!isRadiantTurn && side === 'Dire') {
+            array.push('title__active-dire')
+        }
+        return array
+    }, [selectedPick, pickQueue])
+
 
     function getSide(): number[] {
         return side === 'Radiant' ? PickOrder.radiant : PickOrder.dire
@@ -14,7 +30,7 @@ const PickSide: FC<PickSideProps> = ({side}) => {
 
     return (
         <div className='pick-side'>
-            <h1 className='pick-side__title'>{side}</h1>
+            <h1 className='pick-side__title'><span className={sideTitleClasses.join(' ')}>{side}</span></h1>
             <div className='pick-side__body'>
                 {getSide().map((block) =>
                     <PickBlock key={block} orderNumber={block}/>
