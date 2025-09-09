de597f1ba50f1764bb4c1495c1ec01b25749e181
jijimka <jjeam6thegod@mail.ru>
2025-09-09 15:52:39 +0500

    ver 2

---
+++ src/components/UI/Background/Background.tsx
@@ -0,0 +1,11 @@
+import classes from './Background.module.css'
+
+const Background = () => {
+    return (
+        <div className={classes.background}>
+
+        </div>
+    );
+};
+
+export default Background;
--- src/routes/CaptainsMode.tsx
+++ src/routes/CaptainsMode.tsx
@@ -3,12 +3,13 @@ import AllHeroesList from "../components/captainsMode/AllHeroesList.tsx";
 import PickBar from "../components/captainsMode/PickBar.tsx";
 import SearchInput from "../components/captainsMode/SearchInput.tsx";
 import CaptainsModeSettings from "../components/captainsMode/CaptainsModeSettings.tsx";
-
+import Background from '../components/UI/Background/Background.tsx'
 
 const CaptainsMode = () => {
 
     return (
         <div className="CaptainsMode">
+            <Background/>
             <SearchInput>
                 <div className='captainsMode__body'>
                 <NavMenu/>
