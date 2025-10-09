8afebd3c4d74a347afe953726f731d1a51f7981c
jijimka <jjeam6thegod@mail.ru>
2025-10-09 19:25:31 +0500

    fix

--- src/models/PageRoutes.ts
+++ src/models/PageRoutes.ts
@@ -1,4 +1,4 @@
 export enum PageRoutes {
-    IMMORTALDRAFTURL = '/Dota-Captains-Mode/ImmortalDraft',
-    CAPTAINSMODEURL = '/Dota-Captains-Mode',
+    IMMORTALDRAFTURL = '/ImmortalDraft',
+    CAPTAINSMODEURL = '/',
 }
