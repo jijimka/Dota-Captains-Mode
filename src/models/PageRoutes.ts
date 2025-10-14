27d383d5b0088075496cc4c12d257e7f385b9990
jijimka <jjeam6thegod@mail.ru>
2025-10-14 17:35:26 +0500

    test

--- src/models/PageRoutes.ts
+++ src/models/PageRoutes.ts
@@ -1,5 +1,4 @@
 export enum PageRoutes {
     IMMORTALDRAFTURL = '/Dota-Captains-Mode/ImmortalDraft',
     CAPTAINSMODEURL = '/Dota-Captains-Mode',
-    CAPTAINSMODEURL_MOBILE = '/Dota-Captains-Mode/mobile',
 }
